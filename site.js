c88f23ec6b29104fb54ae808f50ab3b7c855e8fd
Alina Zaman <122azaman@frhsd.com>
2022-03-22 12:26:05 -0400

    c

--- site.js
+++ site.js
@@ -34,6 +34,7 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title: "IMDB + Alina's Top 8 Movies",
       }
     },
       methods: {
