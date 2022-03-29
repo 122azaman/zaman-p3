a88d7a7d419306c488f79017febd13a86f09feb3
Alina Zaman <122azaman@frhsd.com>
2022-03-29 13:47:33 -0400

    setp 6 - date

--- site.js
+++ site.js
@@ -41,9 +41,48 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-            getMonthText() {
-
+            getMonthText(dateArray) {
+              switch(dateArray) {
+                case 0:
+                month = "January";
+                break;
+                case 1:
+                month = "February";
+                break;
+                case 2:
+                month = "March";
+                break;
+                case 3:
+                month = "April";
+                break;
+                case 4:
+                month = "May";
+                break;
+                case 5:
+                month = "June";
+                break;
+                case 6:
+                month = "July";
+                case 7:
+                month = "August";
+                break;
+                case 8:
+                month = "September";
+                break;
+                case 9:
+                month = "October";
+                break;
+                case 10:
+                month = "November";
+                break;
+                case 11:
+                month = "December";
+                break;
+              }
+            return month + " " dateArray[2] + ", " dateArray[0] ;
             }
+
+
       }
 })
 
